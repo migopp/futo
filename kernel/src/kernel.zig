a27d6dccc7807b8c9a18ddc3b1dad20f6eaf1dd9
Michael Goppert <goppert@utexas.edu>
2025-04-26 22:08:05 -0500

    Stack comments

--- kernel/src/kernel.zig
+++ kernel/src/kernel.zig
@@ -1,5 +1,8 @@
-const init = @import("init.zig");
+// const init = @import("init.zig");
 
+/// Kernel stacks.
+///
+/// These need to exist somewhere so that we can start running interesting code.
 const max_cores: comptime_int = 4;
 const kernel_stack_sz: comptime_int = 4 * 4096;
 var kernel_stacks: [max_cores][kernel_stack_sz]u8 align(16) linksection(".bss") = undefined;
